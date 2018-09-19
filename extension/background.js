ba9121f36185710f97464a9c16bcf41182b123d6
Danny Guo <dannyguo91@gmail.com>
2018-09-18 22:49:22 -0400

    Implement the redirect

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,4 @@
-if (
-    typeof window.browser === 'undefined' &&
-    typeof window.chrome === 'object'
-) {
-    window.browser = window.chrome;
-}
+window.browser = window.browser || window.chrome;
 
 function handleRequest(details) {
     if (details.method !== 'GET') {
@@ -12,13 +7,25 @@ function handleRequest(details) {
 
     const url = new URL(details.url);
 
-    // console.log(url);
-
-    if (url.hostname === 'www.amazon.com') {
-        return {
-            redirectUrl: 'https://smile.amazon.com'
-        };
+    // AmazonSmile is only active in Germany, the U.S., and the U.K.
+    const matches = url.hostname.match(/^(?:www\.)?amazon\.(de|com|co\.uk)$/);
+    if (!matches) {
+        return {};
     }
+
+    const extension = matches[1];
+
+    const filters = new RegExp('/ap/|redirect=true');
+
+    if (filters.test(url.href)) {
+        return {};
+    }
+
+    return {
+        redirectUrl: `https://smile.amazon.${extension}${url.pathname}${
+            url.search
+        }`
+    };
 }
 
 const filter = {
