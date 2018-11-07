c03b978262cbd51a4d99e39672d25590486bac7d
Danny Guo <dannyguo91@gmail.com>
2018-11-06 23:08:18 -0500

    Make the icon grey if the redirect is turned off

--- extension/toggle.js
+++ extension/toggle.js
@@ -1,11 +1,19 @@
 window.browser = window.browser || window.chrome;
 
+function setIcon(redirect) {
+    browser.browserAction.setIcon({
+        path: `images/icon-128${redirect ? '' : '-greyscale'}.png`
+    });
+}
+
 browser.storage.local.get('redirect').then(results => {
     document.getElementById('redirect').checked = results.redirect;
+    setIcon(results.redirect);
 });
 
 document.getElementById('redirect').addEventListener('change', event => {
     browser.storage.local.set({redirect: event.target.checked});
+    setIcon(event.target.checked);
 });
 
 browser.storage.onChanged.addListener(changes => {
